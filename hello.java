5e9b32a6931276e6f2774e8e37fb1357c88c40bb
Subekshya Kayastha <subekshyakayastha4@gmail.com>
2023-04-10 14:47:32 +0545

    Week 1

---
+++ hello.java
@@ -0,0 +1,44 @@
+import javax.sound.midi.SysexMessage;
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.SystemMenuBar;
+
+// public class hello {
+//     public static void main (String[] args){
+//         System.out.println("Hello world");
+//         System.out.print("This is my first program");
+//         System.out.print("\nI am on module ST4003CEM");
+//     }
+// }
+
+
+// public class hello{
+//     public static void main (String[] args) {
+//         System.out.println("\t   \\/");
+//         System.out.println("\t  \\\\//");
+//         System.out.println("\t \\\\\\///");
+//         System.out.println("\t ///\\\\\\");
+//         System.out.println("\t  //\\\\");
+//         System.out.println("\t   /\\");
+//     }
+// }
+
+
+// public class hello {
+//     public static void main(String[] args){
+//         System.out.println("*");
+//         System.out.println("**");
+//         System.out.println("***");
+//         System.out.println("****");
+//     }
+// }
+
+
+// public class hello{
+//     public static void main (String[] args){
+//         System.out.println("   *");
+//         System.out.println("  ***");
+//         System.out.println(" ******");
+//         System.out.println("********");
+//     }
+// }
+
+
---
+++ hi.java
@@ -0,0 +1,14 @@
+// public class hi { //class is created
+//     public static void main(String[] args) {
+//         System.out.println("Hello, World of Programming");
+//     }
+
+// }
+
+
+// public class hi { //class is created
+//     public static void main(String[] args) {
+//         System.out.println("Subekshya//C33A//OOP");
+//     }
+// }
+
