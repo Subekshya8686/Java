ed00975e3173e74de0777db86e07c1bb6234fb42
Subekshya Kayastha <subekshyakayastha4@gmail.com>
2023-04-18 09:19:52 +0545

    week 2

---
+++ scanner.java
@@ -0,0 +1,89 @@
+// import java.util.Scanner; // needed for the Scanner class 
+
+
+// public class scanner{
+//     public static void main(String[] args) {
+//         // Scanner class is created along with its object
+
+//         Scanner input = new Scanner(System.in);
+
+        // System.out.print("Enter the value of a: ");
+        // int a= input.nextInt(); //input is read
+
+        // System.out.print("Enter the value of b: ");
+        // int b= input.nextInt();  // or simply take input
+        // int add = a+b;
+        // int product = a*b;
+        // int div = product/add;
+
+        // //output of the input values
+        // System.out.println("The value of a + b: "+ add);
+        // System.out.println("The value of a x b: "+ product);
+        // System.out.println("The division between sum and product: "+ div);
+
+        // input.close(); 
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+        // System.out.print("Enter your name: ");
+        // String name = input.nextLine();
+
+        // System.out.println("Enter your age: ");
+        // int age = input.nextInt();
+
+        // System.out.println("Enter your salary: ");
+        // double salary = input.nextDouble();
+
+        // // output of the input values
+        // System.out.println("Name: "+ name);
+        // System.out.println("Age: "+ age);
+        // System.out.println("Salary: "+ salary);
+
+//////////////////////////////////////////////////////////////////////////////////////////
+
+        // String name; //to hold a name
+        // int hours; // hours worked
+        // double payRate; //hourly pay rate
+        // double grossPay; //gross pay
+
+        // // Create a Scanner object to read input
+        // Scanner keyboard = new Scanner(System.in);
+        
+        // // get the user's name
+        // System.out.print("What is your name? ");
+        // name = keyboard.nextLine();
+    
+        // System.out.print("How many hours do you work? ");
+        // hours = keyboard.nextInt();
+
+        // System.out.print("What is your pay rate? ");
+        // payRate = keyboard.nextDouble();
+
+        // grossPay = hours* payRate; 
+        
+
+        // System.out.println("Name: "+ name);
+        // System.out.println("You work "+ hours+ "Hours");
+        // System.out.println("Your total pay is " + grossPay);
+
+
+        // keyboard.close();
+        
+        //////////////////////////////////////////////////////////////////////////////////////////
+        
+        
+        // System.out.print("Enter your name: ");
+        // String name = input.nextLine(); 
+        
+        // System.out.print("Enter your roll number: ");
+        // int roll = input.nextInt();
+
+        // System.out.print("Your field of interest: ");
+        // String field = input.next();
+
+        // System.out.println("Hey, my name is "+ name + " and my roll number is " + roll+ ". My field of interest is "+ field);
+        
+        // input.close(); //scanner class is closed
+
+//     }
+// }
